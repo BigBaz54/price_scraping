f0a9a7a30759e27d3578cbafbf42f011a4b2c724
BigBaz <louisvincent@famillecapelli.fr>
2023-06-12 18:15:32 +0200

    [+] reading price while avoiding sales

--- carrefour/carrefour.py
+++ carrefour/carrefour.py
@@ -19,8 +19,8 @@ def get_product_page(driver, url):
 def read_price(driver):
     price = driver.execute_script(
         """
-            price = document.querySelectorAll('#data-produit-card > div.main-details__pricing > div > div > div > div.pdp-pricing__block-left > div > div > span')[0].innerText;
-            return price;
+            prices = document.querySelectorAll('#data-produit-card > div.main-details__pricing > div > div > div > div.pdp-pricing__block-left > div > div > :last-child')[0].innerText;
+            return prices;
         """
     )
     price = float(price.replace(',', '.').replace('€', '').strip())
@@ -98,13 +98,11 @@ def get_all_prices(driver, product_url, store_ids, write_to_file=False):
 
 if __name__ == '__main__':
     driver = init_driver()
-    # # store_ids = get_all_store_ids(driver, write_to_file=True)
-    # with open(os.path.join('carrefour', 'store_ids.json'), 'r') as f:
-    #     store_ids = json.load(f)
-    # prices = get_all_prices(driver, 'https://www.carrefour.fr/p/liqueur-get-27-7610113019214', store_ids, write_to_file=True)
-    # print("Prices found: ", len(prices))
-    # print("Min price: ", min(prices))
-    # print("Average price: ", np.mean(prices))
-    # print("Standard deviation: ", np.std(prices))
-    get_product_page(driver, 'https://www.carrefour.fr/p/liqueur-get-27-7610113019214')
-    get_all_store_ids(driver, write_to_file=True)
+    # store_ids = get_all_store_ids(driver, write_to_file=True)
+    with open(os.path.join('carrefour', 'store_ids.json'), 'r') as f:
+        store_ids = json.load(f)
+    prices = get_all_prices(driver, 'https://www.carrefour.fr/p/liqueur-get-27-7610113019214', store_ids, write_to_file=True)
+    print("Prices found: ", len(prices))
+    print("Min price: ", min(prices))
+    print("Average price: ", np.mean(prices))
+    print("Standard deviation: ", np.std(prices))
